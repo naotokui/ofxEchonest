fb9f6a3bf2d03e1f571674875f50c8515cdd9147
Nao Tokui <tokui (at) qosmo.jp>
2015-01-14 13:07:31 +0900

    added: constant for timbre vector size

--- src/ENAnalysisResult.h
+++ src/ENAnalysisResult.h
@@ -12,6 +12,20 @@
 #include <stdio.h>
 #include "ofxJSONElement.h"
 
+// Echonest gives back 12 coefficient values to represent a given timbre
+// --------------------------
+// "timbre is the quality of a musical note or sound that distinguishes different types of musical instruments, or voices.
+// It is a complex notion also referred to as sound color, texture, or tone quality, and is derived from the shape of a segment’s spectro-temporal surface,
+// independently of pitch and loudness. The Echo Nest Analyzer’s timbre feature is a vector that includes 12 unbounded values roughly centered around 0.
+// Those values are high level abstractions of the spectral surface, ordered by degree of importance. For completeness however,
+// the first dimension represents the average loudness of the segment; second emphasizes brightness; third is more closely correlated to the flatness of a sound;
+// fourth to sounds with a stronger attack; etc....   The actual timbre of the segment is best described as a linear combination of these 12 basis functions weighted by the coefficient values:
+//  timbre = c1 x b1 + c2 x b2 + ... + c12 x b12, where c1 to c12 represent the 12 coefficients and b1 to b12 the 12 basis functions as displayed below.
+// Timbre vectors are best used in comparison with each other."
+// http://developer.echonest.com/docs/v4/_static/AnalyzeDocumentation.pdf
+
+static const int kTimbreVectorSize  = 12;
+
 class ENAnalysisResult{
     
 public:
